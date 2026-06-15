41567877b60c1993779085190fb70259ac71a845
K-M-Shehan <miweshshehan@gmail.com>
2026-06-15 19:51:09 +0530

    check functionality of operations

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let operand1 = parseInt(prompt("Enter a number"));
+let operator = prompt("Enter an operator");
+let operand2 = parseInt(prompt("Enter a number"));
+
 function add(operand1, operand2) {
   return operand1 + operand2;
 }
@@ -13,3 +17,30 @@ function multiply(operand1, operand2) {
 function divide(operand1, operand2) {
   return operand1 / operand2;
 }
+
+function operate(operand1, operand2, operator) {
+  let answer = 0;
+  switch (operator) {
+    case "+":
+      answer = add(operand1, operand2);
+      console.log(answer);
+    break;
+    case "-":
+      answer = subtract(operand1, operand2);
+      console.log(answer);
+    break;
+    case "*":
+      answer = multiply(operand1, operand2);
+      console.log(answer);
+    break;
+    case "/":
+      answer = divide(operand1, operand2);
+      console.log(answer);
+    break;
+    default:
+      console.log("Operation invalid\n");
+    break;
+  }
+}
+
+operate(operand1, operand2, operator);
